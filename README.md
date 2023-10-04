e8006f6007348c1d96daca17917324f22488337d
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2023-10-04 12:18:58 +0700

    Inital commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# I use Arch, BTW
+
+Simply because I use Arch, so BTW.
---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+document.querySelector("h1").onmouseover = event => {
+let iterations = 0;
+  const interval = setInterval(() => {
+    event.target.innerHTML = event.target.innerHTML.split("")
+      .map((letter, index) => {
+      if (index < iterations) {
+        return event.target.dataset.value[index];
+      }
+      return letters[Math.floor(Math.random() * 26)]
+    })
+      .join("");
+    if(iterations >= event.target.dataset.value.length) {
+        clearInterval(interval);
+    }
+    iterations += 1 /7;
+  },30);
+}
